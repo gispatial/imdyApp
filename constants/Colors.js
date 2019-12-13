585cc1180c621c91ab3b584b6bf1ee251d803c46
JustEd <ed@smarttechtank.com>
2019-12-14 03:54:58 +0800

    R/N Production
    TestBuildDocumentation on Expo

---
+++ constants/Colors.js
@@ -0,0 +1,14 @@
+const tintColor = '#2f95dc';
+
+export default {
+  tintColor,
+  tabIconDefault: 'orange',
+  tabIconSelected: tintColor,
+  tabBar: 'teal',
+  errorBackground: 'orange',
+  errorText: '#fff',
+  warningBackground: '#EAEB5E',
+  warningText: '#666804',
+  noticeBackground: tintColor,
+  noticeText: '#fff',
+};
